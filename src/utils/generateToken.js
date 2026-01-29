1ce9fafc6ebf0b8487ced92bd28c365006da30c1
Debina <debinabaraili94@gmail.com>
2026-01-29 15:07:19 +0545

    small changes on generate token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,15 +1,13 @@
 import jwt from "jsonwebtoken";
 import { envConfig } from "../Config/envConfig.js";
 
-export const generateToken = (data,res)=>{
-    console.log(data)
-    const token = jwt.sign({_id:data?._id},envConfig?.jwtSecretToken,{expiresIn:"3d"});
+export const generateToken = (token,res)=>{
+    // const token = jwt.sign({_id:data?._id},envConfig?.jwtSecretToken,{expiresIn:"3d"});
     res.cookie("jwt",token,{
         maxAge:24*60*60*1000,
         httpOnly: true,
-        // sameSite: "strict",
+        sameSite: "strict",
         secure:"false"
     });
     return token;
 }
-
